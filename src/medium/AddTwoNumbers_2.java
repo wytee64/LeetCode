1c8e6474eb6c8658aa5e74d2c14e1ae0cc5e493d
raplowytee <raplowwytee@gmail.com>
2025-04-08 21:52:43 +0200

    2 updated wit better code

--- src/medium/AddTwoNumbers_2.java
+++ src/medium/AddTwoNumbers_2.java
@@ -9,52 +9,30 @@ class ListNode {
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 
-
 public class AddTwoNumbers_2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        StringBuilder sb = new StringBuilder();
-        ListNode current = l1;
-        while (current != null) {
-            sb.append(current.val);
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, current = dummyHead;
+        int carry = 0;
+
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+
+            int sum = carry + x + y;
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
             current = current.next;
-        }
-        int num = Integer.parseInt(sb.toString());
-        StringBuilder sb1 = new StringBuilder();
 
-
-        ListNode current1 = l2;
-        while (current1 != null) {
-            sb1.append(current1.val);
-            current1 = current1.next;
-        }
-        int num2 = Integer.parseInt(sb1.toString());
-
-        return numberToLinkedList(num+num2);
-
-
-
-
-    }
-    public static ListNode numberToLinkedList(int number) {
-        String numStr = String.valueOf(number);
-        ListNode newnew = new ListNode(0);
-        ListNode current = newnew;
-
-        for (char digitChar : numStr.toCharArray()) {
-            int digit = digitChar - '0';
-            current.next = new ListNode(digit);
-            current = current.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
         }
 
-        ListNode finalL = null;
-        ListNode currenty = newnew.next;
-        while (currenty != null) {
-            ListNode nextTemp = currenty.next;
-            currenty.next = finalL;
-            finalL = currenty;
-            currenty = nextTemp;
+        if (carry > 0) {
+            current.next = new ListNode(carry);
         }
 
-        return finalL;
+        return dummyHead.next;
     }
 }
