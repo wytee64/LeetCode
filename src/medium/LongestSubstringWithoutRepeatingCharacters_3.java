197801e6eac4d353fc09524464279f88bfd1f18c
raplowytee <raplowwytee@gmail.com>
2025-04-09 15:12:20 +0200

    3

---
+++ src/medium/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -0,0 +1,20 @@
+package medium;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters_3 {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
